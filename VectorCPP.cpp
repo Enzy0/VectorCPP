20dbda0b4a6cdf8dbe8c71ffe44d2e3ac4cc528c
alex <gta4230992003@gmail.com>
2022-09-29 10:28:35 +0300

    Добавьте файлы проекта.

---
+++ VectorCPP.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <iterator>
+#include <cstdlib>
+
+using namespace std;
+int sum(int j, vector<int>& a)
+{
+	int summPos = 0;
+	int summNeg = 0;
+	for (int i = 0; i < j; i++) {
+		if (a[i] > 0) {
+			summPos += a[i];
+		}
+		if (a[i] < 0) {
+			summNeg += a[i];
+		}
+	}
+	cout << "Sum Positive= " << summPos << "\n";
+	cout << "Sum Negative = " << summNeg << "\n";
+	return 0;
+}
+
+int main(int i)
+{
+	int j = 1 + (rand() % 99);
+	vector<int>  v(j);
+	for (i = 0; i < j; i++) {
+		v[i] = (rand() % 200) - 100;
+		cout << i << " = " << v[i] << "\n";
+	}
+	sum(j, v);
+	return 0;
+}
